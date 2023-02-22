7c5d0e8bdc0f2c43fefc32ae639c92ae5e832d2a
anderaddiction <anderaddiction@gmail.com>
2023-02-22 16:34:30 -0400

    Creando estructura de Tramites de Certificados

---
+++ app/Http/Controllers/Tramites/TramiteDiplomaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Tramites;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TramiteDiplomaController extends Controller
+{
+    //
+}
