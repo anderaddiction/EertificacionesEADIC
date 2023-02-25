09b091fa6a6d2aafe6440fa7ddef3e983a7de550
Diego <eadiccoordinadorit1@gmail.com>
2023-02-25 17:48:33 -0400

    Botones de contacto

--- resources/views/soporte/links/index.blade.php
+++ resources/views/soporte/links/index.blade.php
@@ -1,20 +1,186 @@
 @extends('layouts.app')
 
 @section('content_header')
-<h1>Soporte</h1>
+<div>
+
+</div>
 @stop
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Soporte Alumno') }}</div>
-                <div class="card-body">
-                    {{ __('You are logged in!') }}
+    <div class="row justify-content-md-center pb-5">
+
+
+
+
+
+<div class="col-md-8 col-12">
+
+
+    <div class="sticky-top mb-3">
+        <div class="card">
+            
+
+            <div class="card-body">
+
+
+                <div class="row">
+
+                    <img src="https://eadic.org/portal-admisiones/public/img/Banner_portal-de-admisiones.png" alt="" style="width: 100%">
                 </div>
+                
+             
+
+                <div class="row justify-content-md-center mt-3">
+
+                                                    
+
+            
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/gWndvxJdZHc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>
+
+
+
+                    
+                   
+
+
+
+
+                </div>
+
+
+        
+
+                  
+
+
+            
+
+
             </div>
+
+          
+
+
         </div>
+
+    </div>
+
+</div>
+
+
+
+
+<div class="col-md-3 col-12">
+
+<div class="row">
+
+<div class="col-12 mb-1">
+    <div class="info-box">
+      
+
+        <div class="info-box-content">
+        <h5 class="mb-3"><b>Contacta con Nosotros</b> </h5>
+
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+</div>
+
+
+
+
+
+
+
+<!-- /.col -->
+<div class="col-12 mb-1">
+    <div class="info-box">
+        <span class="info-box-icon bg-info" style="padding: 12px;">
+
+            <img src="https://eadic.org/portal-admisiones/public/img/Recurso-7.png" alt="">
+
+        </span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">Escríbenos a WhatsApp</span>
+
+            <a href="https://api.whatsapp.com/send?phone=34919994747&amp;text=Buen%20d%C3%ADa%20equipo%20EADIC.%20Necesito%20asesor%C3%ADa%20con%20mi%20proceso%20de%20admisi%C3%B3n%20al%20M%C3%A1ster" target="_blank">
+                <button type="button" class="btn btn-block bg-gradient-warning btn-sm">Iniciar
+                    chat</button></a>
+
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+</div>
+<!-- /.col -->
+
+
+
+
+
+<div class="col-12 mb-1">
+    <div class="info-box">
+        <span class="info-box-icon bg-info" style="padding: 12px;">
+            <img src="https://eadic.org/portal-admisiones/public/img/Recurso-5.png" alt="">
+        </span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">Preguntas Frecuentes</span>
+            <a href="https://eadic.com/faqs/" target="_blank"><button type="button" class="btn btn-block bg-gradient-info btn-sm">Consultar FAQ</button>
+            </a>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+</div>
+<!-- /.col -->
+
+
+
+
+
+<div class="col-12 mb-1">
+    <div class="info-box">
+        <span class="info-box-icon bg-info" style="padding: 12px;">
+            <img src="https://eadic.org/portal-admisiones/public/img/zoom.png" alt="">
+        </span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">Hablemos por Zoom</span>
+            <a href="https://us06web.zoom.us/j/93151603423" target="_blank"><button type="button" class="btn btn-block bg-gradient-info btn-sm">Iniciar Video llamada</button>
+            </a>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+</div>
+<!-- /.col -->
+
+
+
+
+
+
+
+<div class="col-12 mb-1">
+    <div class="info-box">
+
+        <a href="https://eadic.com/faqs/" target="_blank">
+        <img src="https://eadic.org/portal-admisiones/public/img/GIF-EADIC.gif" alt="" style="  width:100%;">
+    </a>
+    </div>
+
+</div> 
+
+
+</div>
+</div>
+</div>
     </div>
 </div>
 @endsection
