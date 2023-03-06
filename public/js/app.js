3b83a0129fa373b5bd9049d05f2148aca93fcc34
anderaddiction <anderaddiction@gmail.com>
2023-03-06 09:53:59 -0400

    Actualizando Js

--- public/js/app.js
+++ public/js/app.js
@@ -40019,7 +40019,7 @@ Popper.Defaults = Defaults;
 
 "use strict";
 /* provided dependency */ var Buffer = __webpack_require__(/*! buffer */ "./node_modules/buffer/index.js")["Buffer"];
-// Axios v1.3.3 Copyright (c) 2023 Matt Zabriskie and contributors
+// Axios v1.3.4 Copyright (c) 2023 Matt Zabriskie and contributors
 
 
 function bind(fn, thisArg) {
@@ -41235,6 +41235,8 @@ var URLSearchParams$1 = typeof URLSearchParams !== 'undefined' ? URLSearchParams
 
 var FormData$1 = typeof FormData !== 'undefined' ? FormData : null;
 
+var Blob$1 = typeof Blob !== 'undefined' ? Blob : null;
+
 /**
  * Determine if we're running in a standard browser environment
  *
@@ -41289,7 +41291,7 @@ var platform = {
   classes: {
     URLSearchParams: URLSearchParams$1,
     FormData: FormData$1,
-    Blob
+    Blob: Blob$1
   },
   isStandardBrowserEnv,
   isStandardBrowserWebWorkerEnv,
@@ -42637,7 +42639,7 @@ function mergeConfig(config1, config2) {
   return config;
 }
 
-const VERSION = "1.3.3";
+const VERSION = "1.3.4";
 
 const validators$1 = {};
 
