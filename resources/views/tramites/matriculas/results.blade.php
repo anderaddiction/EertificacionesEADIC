5c1a9ebe1022c631c5c7fe634f4d32c71561a9c1
anderson <anderaddiction@gmail.com>
2023-05-10 16:41:32 -0400

    Correccion de titulos

--- resources/views/tramites/matriculas/results.blade.php
+++ resources/views/tramites/matriculas/results.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content_header')
-<h1>Estado de Trámite de Diploma</h1>
+<h1>Estado de Trámite de Matriculación</h1>
 @stop
 
 @section('content')
@@ -9,7 +9,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card card-info">
-                <div class="card-header">{{ __('Conoce el status de tu diploma y/o certificado de notas') }}</div>
+                <div class="card-header">{{ __('Conoce el status de tu matriculación') }}</div>
                 <div class="card-body">
                 <div class="form-group row">
                     <label for="inputEmail3" class="col-sm-2 col-form-label">DNI</label>
