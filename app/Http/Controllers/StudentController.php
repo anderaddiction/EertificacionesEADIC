d4e95a555658832cbbd8da87799d50e43dba1bde
anderson <anderaddiction@gmail.com>
2023-06-28 16:16:43 -0400

    Ajustando el login a dos tablas

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\AlumnoActivo;
 use App\TramiteDiploma;
 use App\Http\Requests\StudentAuthRequest;
 
@@ -16,10 +17,12 @@ class StudentController extends Controller
     {
         $email = $request->email;
         $result = TramiteDiploma::where('correo', $email)->first();
+        $result_activo = AlumnoActivo::where('correo', $email)->first();
+        //return $result_activo;
 
-        if (!empty($result)) {
+        if ((!empty($result)) || (!empty($result_activo))) {
             return view('home');
-        } elseif (!$result) {
+        } elseif ((!$result) || (!$result_activo)) {
             return redirect()->back()->with('alert', '
 Si después de verificar los puntos anteriores y continúas presentando inconvenientes para acceder a nuestro portal, por
                 favor, solicita ayuda a través de la plataforma Soporte EADIC dando clic <a href="http://soporte.eadic.biz/open.php" target="_BLANK"><strong>AQUÍ</strong></a>');
