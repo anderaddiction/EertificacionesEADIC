19a74697de8fe5bec59a6bb8123aaada0c4fbe6b
Anderson Gonzalez <140626142+Anderson-Gonzalez-eadic@users.noreply.github.com>
2023-07-31 15:55:33 -0400

    Mapa de calor

--- resources/views/oficina-vitual/index.blade.php
+++ resources/views/oficina-vitual/index.blade.php
@@ -2,6 +2,17 @@
 
 @section('content_header')
 <img class="main-banner" src="{{ asset("img/main_banner.png") }}" alt="main_banner">
+<!-- Hotjar Tracking Code for https://eadic.org/secretaria_academica/public/oficina-virtual/oficina-virtual -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3591928,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 @stop
 
 @section('content')
